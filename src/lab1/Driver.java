e6814b5e3f7e1eaa8d674e959f04e812928556d9
42null <averyparanoiduser@gmail.com>
2022-11-16 20:19:38 -0600

    Added 2 comments to Driver

--- src/lab1/Driver.java
+++ src/lab1/Driver.java
@@ -12,13 +12,20 @@ public class Driver {
 
 	public static void main(String args[]){
 
+		/*For the architecture relating to abstraction and inheritance, this project I think
+		it is good that because they are so similar to each other that once I created the Course
+		class I was also able to extend that with PrerequisiteRequiringCourse and then extend
+		that again for the others that needed it. The bad that I thought of the way I did it is
+		that there are subclasses of superclasses with only a call to super, if everything is the
+		same I wounder why we even need different classes aka IntroJavaCourse and AdvancedJavaCourse
+		if they have the exact same contents and the only difference is the name.*/
+
+		Course[] courses = new Course[3];
 		/*The advantage of doing it like this is that everything can be stored together so
 		there can be one list to store everything, however, it makes it trickier to call more
 		unique operations without causing a crash. As long as nothing new is added to the
 		implementation by the children then this is perfect but if not then you might need messy
 		checks to avoid crashing*/
-
-		Course[] courses = new Course[3];
 
 		courses[0] = new IntroToProgrammingCourse("IntoToProgramming", "42");
 		courses[1] = new IntroJavaCourse("IntroJava", "43");
--- src/lab2/Driver.java
+++ src/lab2/Driver.java
@@ -18,12 +18,19 @@ public class Driver {
 
 	public static void main(String args[]){
 
+
+		/*For the architecture relating to abstraction and inheritance for lab2 there was a lot of copy and pasting,
+		I feel that if there were many more this would not be a good idea and because courses will probably be mostly
+		similar in structure, having more non-abstract classes would improve efficiency and prevent mistakes from
+		copy+paste spam. For my implementation I feel that to keep with the prompt only creating one interface was
+		enough, however I feel I could have also done it more like lab1 with an interface on top of another for the ones requiring
+		*/
+
 		/*The advantage of doing it like this is that everything can be stored together so
 		there can be one list to store everything, however, it makes it trickier to call more
 		unique operations without causing a crash. As long as nothing new is added to the
 		implementation by the children then this is perfect but if not then you might need messy
 		checks to avoid crashing*/
-
 		lab1.Course[] courses = new lab1.Course[3];
 
 		courses[0] = new IntroToProgrammingCourse("IntoToProgramming", "42");
